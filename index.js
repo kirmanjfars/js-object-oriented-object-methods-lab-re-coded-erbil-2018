76066b5ea3cec6bb339de74f3727216fa441b0c5
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-21 19:51:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-
+function BoardMember(name, address,  job){
+  this.name = name;
+  this.address = address; 
+  this.job = job;
+  return this;
+}
 
 
 polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
